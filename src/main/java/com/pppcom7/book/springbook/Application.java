65a8330aad807f497fab835956c50298a9a33847
pppcom7 <pppcom78@nate.com>
2023-10-21 16:52:45 +0900

    test code를 추가한다

---
+++ src/main/java/com/pppcom7/book/springbook/Application.java
@@ -0,0 +1,13 @@
+package com.pppcom7.book.springbook;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+import static org.springframework.boot.autoconfigure.SpringBootApplication.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        run(Application.class, args);
+    }
+}
